a5e66d325d0e4695eca3c30abf935e849ec6784f
mkouichi <kic.mhr.qs@gmail.com>
2022-09-30 13:49:46 -0400

    Add BaseButton and connect to details component

--- src/components/layout/JournalItems.vue
+++ src/components/layout/JournalItems.vue
@@ -2,16 +2,28 @@
   <li>
     <h2>{{ title }}</h2>
     <p>{{ text }}</p>
+    <div>
+      <BaseButton link :to="detailsLink" mode="outline">View More</BaseButton>
+    </div>
   </li>
 </template>
 
 <script>
 export default {
   props: ["id", "title", "text"],
+  computed: {
+    detailsLink() {
+      return this.$route.path + "/" + this.id;
+    },
+  },
 };
 </script>
 
 <style scoped>
+div {
+  display: flex;
+  justify-content: end;
+}
 li {
   margin: 1.5rem auto;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import router from "./router";
 import store from "./store";
 
 import App from "./App.vue";
+import BaseButton from "./components/ui/BaseButton.vue";
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component("BaseButton", BaseButton);
 
 app.mount("#app");
