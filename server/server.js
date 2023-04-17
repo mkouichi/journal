0d7ad0641dd1b8cc32cb7ad6cbe5e65b0c1077f5
mkouichi <kic.mhr.qs@gmail.com>
2023-04-17 12:26:02 -0400

    Add Firebase config endpoint and move API key variable to global scope

--- server/server.js
+++ server/server.js
@@ -7,6 +7,20 @@ const cors = require("cors");
 const app = express();
 const port = 3000;
 
+// Import the firebase configs from environment variable
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Import the api key from environment variable
+const apiKey = process.env.API_KEY;
+
 app.use(express.json());
 app.use(cors());
 
@@ -14,13 +28,19 @@ app.get("/", (req, res) => {
   return res.status(200).json({ message: "Hello World!" });
 });
 
+// Set up an endpoint that returns the firebase configuration object
+app.get("/firebase-config", (req, res) => {
+  res.json(firebaseConfig);
+});
+
+// Set up an endpoint that returns historical events
 app.get("/historical-events", async (req, res) => {
   const date = new Date();
   const month = date.getMonth() + 1;
   const day = date.getDate();
   const url = `https://api.api-ninjas.com/v1/historicalevents?month=${month}&day=${day}`;
-  console.log(url)
-  const apiKey = process.env.API_KEY;
+  console.log(url);
+
   const options = {
     headers: { "X-Api-Key": apiKey },
     contentType: "application/json",
