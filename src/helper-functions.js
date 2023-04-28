95cc27f7a5767a96724ecd73b01579917f2ee41a
mkouichi <kic.mhr.qs@gmail.com>
2023-04-28 17:57:10 -0400

    Update: Only show entries for the current user - Filter entries with the current user's ID - Change variable name from colRef to journalRef - Add comments

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,35 +1,46 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from "@/firebase";
 
 import store from "./store";
 
-// Collection ref
-const colRef = collection(db, "journal");
+const userId = store.getters.getUserId;
+
+// Define a reference to the "journal" collection
+const journalRef = collection(db, "journal");
+
+// Create a query against the collection
+// Only get entries for the current user
+const q = query(journalRef, where("userId", "==", userId));
 
 // Get data once
 const getDataFromDB = async () => {
   const entries = [];
 
-  // Set loading to true
+  // Set loading state to true
   store.dispatch("dialog/setLoading", true);
 
-  const querySnapshot = await getDocs(colRef);
+  // Retrieve data from the Firestore database using the provided query
+  const querySnapshot = await getDocs(q);
 
+  // Iterate over each document returned by the query and add it to the entries array
   querySnapshot.forEach((doc) => {
     const entry = {
       ...doc.data(),
       // Use the generated id
       id: doc.id,
     };
+
+    // Add the entry to the beginning of the array
     entries.unshift(entry);
   });
 
-  // Set loading to false
+  // Set loading state to false
   store.dispatch("dialog/setLoading", false);
 
-  // Set data to Vuex
+  // Store the retrieved data in Vuex by dispatching a Vuex action
   store.dispatch("journal/setEntryData", entries);
 
+  // Return the retrieved entries
   return entries;
 };
 
