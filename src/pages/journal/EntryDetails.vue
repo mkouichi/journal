b75d57688d157ab78ffdf2cae1c1a558853e5359
mkouichi <kic.mhr.qs@gmail.com>
2023-05-04 21:24:54 -0400

    Migration from the Options API to the Composition API: EntryDetails

--- src/pages/journal/EntryDetails.vue
+++ src/pages/journal/EntryDetails.vue
@@ -44,79 +44,84 @@
   </w-dialog>
 </template>
 
-<script>
-import { mapGetters, mapActions } from "vuex";
+<script setup>
+import { reactive, inject, computed, onBeforeMount } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { useStore } from "vuex";
 import { doc, getDoc, deleteDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 
-export default {
-  async beforeMount() {
-    let entry;
+const store = useStore();
 
-    // Try getting the entry's contents from Vuex using its id
-    entry = this.getEntryById[this.id];
+const route = useRoute();
+const editLink = computed(() => route.path + "/edit");
+const targetEntry = computed(() => store.getters["journal/targetEntry"]);
 
-    // If the entry doesn't exist in Vuex, try finding it in Firebase
-    if (entry === undefined) {
-      const docRef = doc(db, "journal", this.id);
-      const docSnap = await getDoc(docRef);
+const dialog = reactive({
+  show: false,
+  width: "50vw",
+});
 
-      if (docSnap.exists()) {
-        entry = docSnap.data();
-      } else {
-        console.log("No such document!");
-      }
+// This id prop is passed from the route parameter
+const props = defineProps({ id: String });
+
+const setTargetEntry = (payload) =>
+  store.dispatch("journal/setTargetEntry", payload);
+
+onBeforeMount(async () => {
+  const getEntryById = computed(() =>
+    store.getters["journal/getEntryById"](props.id)
+  );
+  let entry;
+
+  // Try getting the entry's contents from Vuex using its id
+  entry = getEntryById.value;
+
+  // If the entry doesn't exist in Vuex, try finding it in Firebase
+  if (entry === undefined) {
+    const docRef = doc(db, "journal", props.id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      entry = docSnap.data();
+    } else {
+      console.log("No such document!");
     }
+  }
 
-    // Set found entry to Vuex
-    this.setTargetEntry(entry);
-  },
-  props: ["id"],
-  data: () => ({
-    dialog: {
-      show: false,
-      width: "50vw",
-    },
-  }),
-  computed: {
-    ...mapGetters({
-      getView: "getView",
-      getEntryById: "journal/getEntryById",
-      targetEntry: "journal/getTargetEntry",
-    }),
+  // Set found entry to Vuex
+  setTargetEntry(entry);
+});
 
-    editLink() {
-      return this.$route.path + "/edit";
-    },
-  },
-  methods: {
-    ...mapActions({
-      setTargetEntry: "journal/setTargetEntry",
-    }),
-    goBack() {
-      // Redirect to current view
-      this.$router.push("/journal/" + this.getView);
-    },
-    async deleteEntry() {
-      await deleteDoc(doc(db, "journal", this.id));
-      this.dialog.show = false;
-      this.$router.push("/journal/" + this.getView);
-      this.notifyPostDeleted();
-    },
-    // Notification
-    notifyPostDeleted() {
-      this.$waveui.notify({
-        lg: true,
-        message: "Post deleted successfully!",
-        timeout: 3000,
-        success: true,
-        plain: true,
-        shadow: true,
-        dismiss: true,
-        transition: "bounce",
-      });
-    },
-  },
+const router = useRouter();
+const getView = computed(() => store.getters.getView);
+
+const goBack = () => {
+  // Redirect to current view
+  router.push("/journal/" + getView.value);
+};
+
+const deleteEntry = async () => {
+  await deleteDoc(doc(db, "journal", props.id));
+  dialog.show = false;
+  router.push("/journal/" + getView.value);
+  notifyPostDeleted();
+};
+
+const $waveui = inject("$waveui");
+
+// Notification
+const notifyPostDeleted = () => {
+  $waveui.notify({
+    lg: true,
+    message: "Post deleted successfully!",
+    timeout: 3000,
+    success: true,
+    plain: true,
+    shadow: true,
+    dismiss: true,
+    transition: "bounce",
+  });
 };
 </script>
 
